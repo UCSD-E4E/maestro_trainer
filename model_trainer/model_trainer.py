b1d99bbef727abd2cbe9ea64f730f8d6ab1325a8
Sean Perry <sean.hyatt.perry@gmail.com>
2024-09-12 10:02:55 -0700

    feat: bring back commication testing

--- model_trainer/model_trainer.py
+++ model_trainer/model_trainer.py
@@ -2,36 +2,36 @@ import random
 import socketio
 
 import time 
-while True: 
-    time.sleep(100)
-    print("loop")
+# while True: 
+#     time.sleep(100)
+#     print("loop")
 
-# print("RUNNING PYTHON!!!", flush=True)
-# sio = socketio.Client()
+print("RUNNING PYTHON!!!", flush=True)
+sio = socketio.Client()
 
-# @sio.event
-# def connect():
-#     print('connection established', flush=True)
-#     sio.emit('job_ready', {'response': 'my response'})
+@sio.event
+def connect():
+    print('connection established', flush=True)
+    sio.emit('job_ready', {'response': 'my response'})
 
-# @sio.on('start_job')
-# def trigger_test(data):
-#     print(data['example_input'], flush=True)
-#     number = random.randrange(0, 100)
-#     print("random number", number, flush=True)
-#     sio.emit('job_done', {'example_output': number})
+@sio.on('start_job')
+def trigger_test(data):
+    print(data['example_input'], flush=True)
+    number = random.randrange(0, 100)
+    print("random number", number, flush=True)
+    sio.emit('job_done', {'example_output': number})
 
-# @sio.event
-# def my_message(data):
-#     print('message received with ', data, flush=True)
-#     sio.emit('my response', {'response': 'my response'})
+@sio.event
+def my_message(data):
+    print('message received with ', data, flush=True)
+    sio.emit('my response', {'response': 'my response'})
 
-# @sio.event
-# def disconnect():
-#     print('disconnected from server', flush=True)   
+@sio.event
+def disconnect():
+    print('disconnected from server', flush=True)   
 
-# sio.connect('https://test-service2.nrp-nautilus.io')
-# sio.wait()
+sio.connect('https://test-service2.nrp-nautilus.io')
+sio.wait()
 
 
 
